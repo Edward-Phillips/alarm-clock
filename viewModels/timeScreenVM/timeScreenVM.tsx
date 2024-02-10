f4038d36d3a6acd43082c626cb37c3d41ecaf8a9
edwardPhillips <edd0ddi0@gmail.com>
2024-02-10 16:08:32 +0000

    feat: add time screen showing current time

--- viewModels/timeScreenVM/timeScreenVM.tsx
+++ viewModels/timeScreenVM/timeScreenVM.tsx
@@ -1,9 +1,32 @@
-import { computed, observable } from 'mobx'
+import { computed, makeAutoObservable, makeObservable, observable, reaction, runInAction } from 'mobx'
+import moment  from 'moment'
 
 export default class timeScreenVM {
 
-  @observable time = moment()
-  constructor () {
+  @observable currentTime = moment()
 
+  constructor() {
+    makeObservable(this);
+
+    reaction(
+      () => this.currentTime,
+      () => {
+        const interval = setInterval(() => {
+          // Wrapping the state update in runInAction
+          runInAction(() => {
+            this.currentTime = moment()
+          });
+        }, 1000);
+        return () => clearInterval(interval); // Cleanup on reaction disposal
+      },
+      {
+        fireImmediately: true,
+      }
+    );
+  }
+
+
+  @computed get formattedTime () {
+    return this.currentTime.format('HH-mm-ss')
   }
 }
--- viewModels/useViewModel.tsx
+++ viewModels/useViewModel.tsx
@@ -1,7 +1,9 @@
+import moment from "moment"
 import { useMemo } from "react"
 
 
 // if we implment a store/rootStore we can add in a second argument here to pass the rootStore as a prop into the VM & add it to the useMemo dependency array.
 export default function useViewModel<T>(VM: new () => T):T {
-  return useMemo(() => new VM(), [])
+  const currentTime = moment()
+  return useMemo(() => new VM(), [currentTime.milliseconds()])
 }
