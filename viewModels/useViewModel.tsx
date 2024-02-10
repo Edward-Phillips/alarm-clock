6f9b861e103cfe919d46e4b32ddddccc83710765
edwardPhillips <edd0ddi0@gmail.com>
2024-02-10 16:12:47 +0000

    refactor: remove silly memo dependency

--- viewModels/useViewModel.tsx
+++ viewModels/useViewModel.tsx
@@ -4,6 +4,5 @@ import { useMemo } from "react"
 
 // if we implment a store/rootStore we can add in a second argument here to pass the rootStore as a prop into the VM & add it to the useMemo dependency array.
 export default function useViewModel<T>(VM: new () => T):T {
-  const currentTime = moment()
-  return useMemo(() => new VM(), [currentTime.milliseconds()])
+  return useMemo(() => new VM(), [])
 }
