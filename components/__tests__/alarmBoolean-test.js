15d95c6b4edf63a42bf03fc3b15fdb59393b4351
AlecDWalker <walkeralec@hotmail.com>
2024-02-22 20:17:33 +0000

    Refactor: moved a timescreen instantiation into each individual test. Tests all still passing

--- components/__tests__/alarmBoolean-test.js
+++ components/__tests__/alarmBoolean-test.js
@@ -1,17 +1,17 @@
 import timeScreenVM from '../../viewModels/timeScreenVM/timeScreenVM'
 import {observable} from 'mobx'
 
-const timescreen = new timeScreenVM()
-
 it('returns false if no alarm set', () => {
+    const timescreen = new timeScreenVM()
 
     expect(timescreen.alarmEnabled).toEqual(false)
 
     timescreen.dispose()
-
 })
 
 it('can toggle alarm status on and off', () => {
+    const timescreen = new timeScreenVM()
+
     timescreen.toggleAlarmEnabled()
 
     expect(timescreen.alarmEnabled).toEqual(true)
