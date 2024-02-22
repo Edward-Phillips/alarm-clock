63cccd0b74efc29962aa5040a27881cb454d785a
AlecDWalker <walkeralec@hotmail.com>
2024-02-22 14:03:23 +0000

    test: added test for toggling alarm status on (not passing)

--- components/__tests__/alarmBoolean-test.js
+++ components/__tests__/alarmBoolean-test.js
@@ -1,13 +1,18 @@
 import timeScreenVM from '../../viewModels/timeScreenVM/timeScreenVM'
 import {observable} from 'mobx'
 
+const timescreen = new timeScreenVM()
 
 it('returns false if no alarm set', () => {
-
-    const timescreen = new timeScreenVM()
 
     expect(timescreen.alarmEnabled).toEqual(false)
 
     timescreen.dispose()
 
+})
+
+it('can toggle alarm status on', () => {
+    expect(timescreen.toggleAlarmEnabled().alarmEnabled).toEqual(true)
+
+    timescreen.dispose()
 })
